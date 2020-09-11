536dc66f8a9204a40247c618dc58f5957e1a0b0e
s2it_csilva <s2it_csilva@uolinc.com>
2020-09-11 09:46:39 -0300

    commit inicial

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/carloscubas/bookstore_user-api/datasources/mysql/users_db"
 	"github.com/carloscubas/bookstore_user-api/utils/date_utils"
 	"github.com/carloscubas/bookstore_user-api/utils/erros"
 )
@@ -11,6 +12,11 @@ var (
 )
 
 func (user *User) Get() *erros.RestErr {
+
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return erros.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
